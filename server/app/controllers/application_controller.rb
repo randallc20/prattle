d5f141277ec38634eed8246a2fa271fc0fafaed5
Allie LeFever <alefever39@gmail.com>
2022-07-08 14:22:25 -0600

    server structure added

---
+++ server/app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationController < Sinatra::Base
+  set :default_content_type, "application/json"
+
+  # get "/messages" do
+  #   Message.all.order(:created_at).to_json
+  # end
+
+  # post "/messages" do
+  #   Message.create(body: params[:body], username: params[:username]).to_json
+  # end
+
+  # patch "/messages/:id" do
+  #   message = Message.find(params[:id])
+  #   message.update(body: params[:body])
+  #   message.to_json
+  # end
+
+  # delete "/messages/:id" do
+  #   message = Message.find(params[:id])
+  #   message.destroy
+  #   message.to_json
+  # end
+end
