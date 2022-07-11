ef4cf818becb1522f45d8fab4df708e202f60c5f
Allie LeFever <alefever39@gmail.com>
2022-07-11 12:47:03 -0600

    hopefully this fixes it

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+import Home from "./components/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Header />}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path="/" element={<Hero />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
